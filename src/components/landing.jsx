6ebd3f6b633340d428f8e73a995e42d11b6d3e7b
Robin Vold <robvold98@gmail.com>
2021-03-17 23:17:05 +0100

    starta på rooms

--- src/components/landing.jsx
+++ src/components/landing.jsx
@@ -1,21 +1,56 @@
 import './landing.css';
 import {useState} from 'react';
+import { io } from "socket.io-client";
+const socket = io("localhost:8000/");
 
 
 function Landing(props) {
-  const [name, setName] = useState("");
+  const [joinRoomId, setJoinRoomId] = useState("");
+  const [makeRoomId, setMakeRoomId] = useState("");
+  
 
-  const handleChange = (evt) => {
-    setName(evt.target.value);
+  const handleChangeJoin = (evt) => {
+    setJoinRoomId(evt.target.value);
+  }
+  
+  const handleChangeMake = (evt) => {
+    setMakeRoomId(evt.target.value);
+  }
+
+  async function scoutRoom(roomId){
+    let res =  await new Promise(resolve => {
+      socket.emit('scout', roomId, (answer) => {
+        resolve(answer);
+      });
+    });   
+    return res;
+  }
+
+  function joinRoom(){
+    if(scoutRoom(joinRoomId)){
+      props.history.push("/rooms/" + joinRoomId);
+    }
+  }
+  
+  async function makeRoom(){
+    if(!(await scoutRoom(makeRoomId))){
+      props.history.push("/rooms/" + makeRoomId);
+    }
   }
 
   return (
     <div className="landing">
         <p>Here you can chat with strangers or friends!</p>
         <h2>Enter your display name to get started</h2>
-        <input type="text" value={name} onChange = {(evt) => handleChange(evt)} ></input>
-        <button onClick={() => props.connect(name)}>Connect!</button>
-    </div>
+        <div>
+          <input type="text" value={joinRoomId} onChange = {(evt) => handleChangeJoin(evt)} ></input>
+          <button onClick={joinRoom}>JOIN ROOM</button>
+        </div>
+        <div>
+          <input type="text" value={makeRoomId} onChange = {(evt) => handleChangeMake(evt)} ></input>
+          <button onClick={makeRoom}>CREATE ROOM</button>
+        </div>
+      </div>
   );
 }
 
