0f2581c2606a795c4597b6c7a81cec16c24bd1a8
Robin Vold <robvold98@gmail.com>
2021-03-23 20:48:02 +0100

    styrer med kommunikasjon, får feil med ice-servere på klienten som ringer

--- src/components/pong.jsx
+++ src/components/pong.jsx
@@ -4,12 +4,13 @@ import { io } from "socket.io-client";
 const socket = io("localhost:8000/");
 
 function Pong(props) {
-    const [id, setId] = useState("");
-    const [connected, setConnected] = useState(false);
-    const [receivingCall, setReceivingCall] = useState(false);
-    const peerRef = useRef();
-    const partnerVideoRef = useRef();
-    const videoStream = useRef();
+    let id = "";
+    let peerConnection = {};
+    // const [connected, setConnected] = useState(false);
+    // const [receivingCall, setReceivingCall] = useState(false);
+    // const peerRef = useRef();
+    // const partnerVideoRef = useRef();
+    // const videoStream = useRef();
 
     //Mine:
     const [awaitingGame, setAwaitingGame] = useState(true);
@@ -21,42 +22,14 @@ function Pong(props) {
         //Initialisere spillogikk??
     },[]);
 
-    const initializeSocket = () => {
-        // socket.emit("my_name_is",props.name);
+    const initializeSocket = () => {       
 
-        // socket.on("you", (myId) => {
-        //     setId(myId);
-        // });
-        // socket.on("peers", (peersConnected) => {
-        //     setPeers(peersConnected);
-        // });
-        // socket.on("offer", async (message) => {
-        //     await receiveCall(message);
-        // });
-
-        // socket.on('candidate', async (message) => { 
-        //     console.log(peerRef.current);           
-        //     if (message.candidate) {
-        //         try {
-        //             await peerRef.current.addIceCandidate(message.candidate);
-        //         } catch (e) {
-        //             console.error('Error adding received ice candidate', e);
-        //         }
-        //     }
-        // });
-
-        // socket.on('answer', async message => {
-        //     if (message.answer) {
-        //         console.log(peerRef.current);
-        //         const remoteDesc = new RTCSessionDescription(message.answer);
-        //         await peerRef.current.setRemoteDescription(remoteDesc);
-        //     }
-        // });
+        
 
         socket.emit("awaitingGame");
 
         socket.on("you", (myId) => {
-            setId(myId);
+            id = myId;
         });
 
         socket.on("gamePartnerId", (partnerId) => {
@@ -71,8 +44,30 @@ function Pong(props) {
             console.log("venter!");
             setAwaitingGame(true);
         });
-
         
+        socket.on('candidate', async (message) => { 
+            console.log(peerConnection);           
+            if (message.candidate) {
+                try {
+                    await peerConnection.addIceCandidate(message.candidate);
+                } catch (e) {
+                    console.error('Error adding received ice candidate', e);
+                }
+            }
+        });
+        
+        socket.on("gameOffer", async (message) => {
+            setAwaitingGame(false);
+            await receiveCall(message);
+        });
+        
+        socket.on('answer', async message => {
+            if (message.answer) {
+                console.log(peerConnection);
+                const remoteDesc = new RTCSessionDescription(message.answer);
+                await peerConnection.setRemoteDescription(remoteDesc);
+            }
+        });
     };
 
     function renderScreen(){
@@ -86,56 +81,42 @@ function Pong(props) {
 
 
     async function makeCall(peerId) {
-        peerRef.current = initializePeerConnection(peerId);
-        // videoStream.current.getTracks().forEach(track => {
-        //     peerRef.current.addTrack(track, videoStream.current);
-        // });
-
-
-        
+        console.log("Denne klienten ringer!!!!")
+        initializePeerConnection(peerId);
+              
         const chatChannel = peerConnection.createDataChannel('chat');
         chatChannel.onmessage = (event) => console.log('onmessage:', event.data);
-        chatChannel.onopen = () => {console.log('onopen')
+        chatChannel.onopen = () => {
+            console.log('onopen')
             chatChannel.send("hola senorita");
             console.log(peerConnection)
         };
         chatChannel.onclose = () => console.log('onclose');
 
-        const offer = await peerRef.current.createOffer();
-        await peerRef.current.setLocalDescription(offer);
-        socket.emit('offer',{"id":peerId, "senderId":id, "offer":offer});
+        const offer = await peerConnection.createOffer();
+        await peerConnection.setLocalDescription(offer);
+        console.log("sendt offer:")
+        socket.emit('gameOffer',{"id":peerId, "senderId":id, "offer":offer});
     }
+
+
 
     const initializePeerConnection = (peerId) => {
         const configuration = {'iceServers': [{'urls': 'stun:stun.l.google.com:19302'}]}
-        const peerConnection = new RTCPeerConnection(configuration);
-
-        peerConnection.addEventListener('iceconnectionstatechange', event => {
-            console.log("dgf: " + event);
-            console.log(peerConnection.iceConnectionState);
-            if (peerConnection.iceConnectionState === 'connected') {
-                setConnected(true);
-                socket.emit("disconnect");
-            }
-        });
-
-        // peerConnection.addEventListener('track', async (event) => {
-        //     console.log("inni listener")
-        //     partnerVideoRef.current.srcObject = event.streams[0]
-        // }); 
-
+        peerConnection = new RTCPeerConnection(configuration);
         peerConnection.onicecandidate = (event) => {
             if (event.candidate) {
-                socket.emit('candidate', {'id': peerId, 'candidate': event.candidate});
+                socket.emit('candidate', {'id': peerId, "senderId":id, 'candidate': event.candidate});
             }
         };
-        return peerConnection;
     }
     
 
+
     const receiveCall = async (message) => {
-        peerRef.current = initializePeerConnection(message.senderId);
-        /*
+        console.log("Denne klienten mottar!!")
+        initializePeerConnection(message.senderId);
+        
         let chatChannel;
         peerConnection.ondatachannel = (event) => {
             if (event.channel.label == 'chat') {
@@ -149,18 +130,16 @@ function Pong(props) {
                 chatChannel.onclose = () => console.log('onclose');
             }
         };
-        */
-
+        
         if (message.offer) {
-            peerRef.current.setRemoteDescription(new RTCSessionDescription(message.offer));
+            peerConnection.setRemoteDescription(new RTCSessionDescription(message.offer));
 
-            videoStream.current.getTracks().forEach(track => {
-                peerRef.current.addTrack(track, videoStream.current);
-            });
+            console.log("Kommet til if inne receiveCall");
 
-            const answer = await peerRef.current.createAnswer();
-            await peerRef.current.setLocalDescription(answer);
-            socket.emit('answer', {"id":message.senderId,"answer":answer});
+            const answer = await peerConnection.createAnswer();
+            await peerConnection.setLocalDescription(answer);
+            console.log(message.senderId);
+            socket.emit('answer', {senderId:id,id:message.senderId,answer:answer});
             
         }   
     }
