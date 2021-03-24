814f476b7aed708ca9ec82aa0a5b907900830af3
Sigmund Granaas <sigmundsgranaas@gmail.com>
2021-03-24 22:15:48 +0100

    * Unmuting all partners

--- src/components/partner.jsx
+++ src/components/partner.jsx
@@ -1,20 +1,24 @@
-import './partner.css';
-import {useEffect, useRef} from 'react';
+import "./partner.css";
+import { useEffect, useRef } from "react";
 
-function Partner(props){
-    const partnerVideo = useRef();
+function Partner(props) {
+  const partnerVideo = useRef();
 
-    useEffect(() => {
-        partnerVideo.current.srcObject = props.peerConnection;
-    },[props.peerConnection]);
+  useEffect(() => {
+    partnerVideo.current.srcObject = props.peerConnection;
+  }, [props.peerConnection]);
 
-    return (
-        <div style={props.styling.divStyle}>
-            <video autoPlay={true} ref={partnerVideo} muted style={props.styling.videoStyle}>
-                    Your browser does not support the video tag.
-            </video>
-        </div>
-    )
+  return (
+    <div style={props.styling.divStyle}>
+      <video
+        autoPlay={true}
+        ref={partnerVideo}
+        style={props.styling.videoStyle}
+      >
+        Your browser does not support the video tag.
+      </video>
+    </div>
+  );
 }
 
 export default Partner;
