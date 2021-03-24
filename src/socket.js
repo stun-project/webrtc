c1d921227fa910a9132784c0b5d8bcbf4d51a785
Sigmund Granaas <sigmundsgranaas@gmail.com>
2021-03-24 21:33:26 +0100

    Adding env's

--- src/socket.js
+++ src/socket.js
@@ -1,5 +1,10 @@
 import { io } from "socket.io-client";
-export const socket = io.connect("localhost:8000/");
-socket.on("disconnect",function(){
-    socket.open();
+//export const socket = io("localhost:8000");
+
+export const socket = io(`${process.env.REACT_APP_SIGNAL_SERVER_URL}/`, {
+  path: `${process.env.REACT_APP_SIGNAL_SERVER_PATH}`,
+});
+
+socket.on("disconnect", function () {
+  socket.open();
 });
